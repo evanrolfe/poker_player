bf6bd4984691ee438cd35e4aea1768089b7fc8a5
Evan Rolfe <evanrolfe@gmail.com>
2014-09-29 15:44:38 +0100

    fixed spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,13 +4,13 @@ describe PokerPlayer do
   it 'PRE FLOP GAME 1' do
   	game_json = load_game('pre_flop_1')
     move = PokerPlayer.get_move(game_json)
-    expect(move).to eq(['bet',2])
+    expect(move).to eq(['bet',6])
   end
 
   it 'PRE FLOP GAME 2' do
     game_json = load_game('pre_flop_2')
     move = PokerPlayer.get_move(game_json)
-    expect(move.first).to eq('call')
+    expect(move).to eq(['bet', 4])
   end  
 
   it 'PRE FLOP GAME 3' do
@@ -34,13 +34,13 @@ describe PokerPlayer do
   it 'FLOP GAME 2' do
     game_json = load_game('flop_2')
     move = PokerPlayer.get_move(game_json)
-    expect(move).to eq(['bet',47])
+    expect(move).to eq(['bet', 14])
   end         
 
   it 'TURN GAME 1' do
     game_json = load_game('turn_1')
     move = PokerPlayer.get_move(game_json)
-    expect(move).to eq(['bet',47])
+    expect(move).to eq(['bet', 11])
   end  
 
   it 'RIVER GAME 1' do
